6899e809b1d093e1e663b1427535b9d42fb2f69f
Alexander Warfield <307286778+MiddleMinded@users.noreply.github.com>
2026-09-24 18:44:04 -0700

    portfolio-tracker: Created get_price in main.py

--- portfolio-tracker/main.py
+++ portfolio-tracker/main.py
@@ -8,5 +8,27 @@ def main():
     user_db = Database(db_path)
     user_portfolio = Portfolio(user_db)
 
+def get_price(ticker: str, current_prices: dict) -> float:
+    """Returns the current price for a given ticker."""
+
+    if ticker in current_prices:
+        return current_prices[ticker]
+    else:
+        print(f"{ticker} does not exist.")
+        temp_price = float(input(f"Enter the current price of {ticker}: $"))
+        current_prices[ticker] = temp_price
+        print(f"\n{ticker} @ ${temp_price:.2f} stored.")
+        return current_prices[ticker]
+         
+
 if __name__ == "__main__":
-    main()
+    current_prices = {
+        "AAPL": 10,
+        "MSFT": 20,
+        "SPCX": 120
+    }
+    ticker = "TSLA"
+    print(get_price(ticker, current_prices))
+    print(get_price(ticker, current_prices))
+
+    
